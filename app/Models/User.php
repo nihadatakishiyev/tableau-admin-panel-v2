4176693795f20e1b60c9e08cb250fe9d50447941
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-01-14 18:05:12 +0400

    installs backpack command line interface and creates user crud

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, CrudTrait;
 
     /**
      * The attributes that are mass assignable.
