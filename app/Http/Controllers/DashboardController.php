734023f1afe33ce979e366cfc61bbc128de0e8bf
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-01-15 15:43:18 +0400

    implements autherization check

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,27 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function asanLoginRealTime(){
-        if (1)
+        $user = Auth::user();
+
+        if ($user->can('AsanLoginRealTime'))
         {
            return view('dashboards.AsanLogin.AsanLoginRealTime');
         }
+        return response()->view('errors.401', [], 401);
     }
 
     public function asanLoginMainPage(){
-        if (1)
+        $user = Auth::user();
+
+        if ($user->can('AsanLoginMainPage'))
         {
             return view('dashboards.AsanLogin.AsanLoginMainPage');
         }
+        return response()->view('errors.401', [], 401);
     }
 
     public function asanFinanceGeneral(){
-        if (1)
+        $user = Auth::user();
+
+        if ($user->can('AsanFinanceGeneral'))
         {
             return view('dashboards.AsanFinance.AsanFinanceGeneral');
         }
+        return response()->view('errors.401', [], 401);
     }
 }
