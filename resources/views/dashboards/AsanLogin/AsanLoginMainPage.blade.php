d66846ebf61cd1a02b0934a3567250576a8cf2cd
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-01-15 15:17:57 +0400

    adds blade contents

--- resources/views/dashboards/AsanLogin/AsanLoginMainPage.blade.php
+++ resources/views/dashboards/AsanLogin/AsanLoginMainPage.blade.php
@@ -4,13 +4,13 @@
 
     <script type='text/javascript' src='http://192.168.20.213/javascripts/api/viz_v1.js'></script>
 
-    <div class='tableauPlaceholder' style='width: 1000px; height: 827px; margin-left: auto; margin-right: auto'>
-        <object class='tableauViz' width='1000' height='827' style='display:none;'>
-            '<param name='host_url' value='http%3A%2F%2F192.168.20.213%2F' />
+    <div class='tableauPlaceholder' style='width: 1500px; height: 950px; margin-left: auto; margin-right: auto'>
+        <object class='tableauViz' width='1500' height='950' style='display:none;'>
+            <param name='host_url' value='http%3A%2F%2F192.168.20.213%2F' />
             <param name='embed_code_version' value='3' />
             <param name='site_root' value='' />
-            <param name='name' value='AsanLoginRealTime&#47;General' />
-            <param name='tabs' value='no' />
+            <param name='name' value='AsanLoginDMver02&#47;sasShif' />
+            <param name='tabs' value='yes' />
             <param name='toolbar' value='no' />
             <param name='showAppBanner' value='false' />
         </object>
