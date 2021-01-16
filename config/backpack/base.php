751cf9a4942f4a800fb83b7f84c99eb4b45a9f17
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-01-16 18:30:40 +0400

    disables backpag login/reg

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -191,7 +191,7 @@ return [
     // Set this to false if you would like to use your own AuthController and PasswordController
     // (you then need to setup your auth routes manually in your routes.php file)
     // Warning: if you disable this, the password recovery routes (below) will be disabled too!
-    'setup_auth_routes' => true,
+    'setup_auth_routes' => false,
 
     // Set this to false if you would like to skip adding the password recovery routes
     // (you then need to manually define the routes in your web.php)
@@ -236,7 +236,7 @@ return [
 
     // The guard that protects the Backpack admin panel.
     // If null, the config.auth.defaults.guard value will be used.
-    'guard' => 'backpack',
+    'guard' => null,
 
     // The password reset configuration for Backpack.
     // If null, the config.auth.defaults.passwords value will be used.
