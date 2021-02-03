98c9ed0efac9f6c19ecef11ec717485869f1c503
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-03 18:31:57 +0400

    adjusts scaling

--- resources/views/pnf.blade.php
+++ resources/views/pnf.blade.php
@@ -5,7 +5,7 @@
         <div class="text" style="margin-left: auto; margin-right: auto; margin-top: 10%">
             <h4 style="">Oh no! Page not found</h4>
             <p>Looks like you hit a bad link somehow</p>
-            <img src="{{asset('photos/status-not-found.svg')}}" alt="" style="margin-top: 3rem">
+            <img src="{{asset('photos/status-not-found.svg')}}" alt="" style="margin-top: 2rem">
         </div>
 
     </div>
