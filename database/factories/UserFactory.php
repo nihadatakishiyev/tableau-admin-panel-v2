1513e683c4622b5113c37aaf65b83fea7826af8d
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-03 11:47:34 +0400

    implements user and dep factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Department;
+use App\Models\Unit;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -25,9 +29,19 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'department_id' => $this->faker->numberBetween(1, Department::count()),
+            'unit_id' => $this->faker->numberBetween(1, Unit::count()),
+            'position_id' => 0,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123123'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user){
+            $user->position_id = $this->faker->randomElement(DB::table('unit_positions')->where('unit_id', $user->unit_id)->get()->toArray());
+        });
     }
 }
