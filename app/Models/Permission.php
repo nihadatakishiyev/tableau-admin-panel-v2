3599d638f9195eb9edc77b1f90717e6944fc541e
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-04 11:56:01 +0400

    implements logging for perm and role

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,11 +3,25 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Models\Permission as OriginalPermission;
 
 class Permission extends OriginalPermission
 {
-    use CrudTrait;
+    use CrudTrait, LogsActivity;
 
     protected $fillable = ['name', 'guard_name', 'updated_at', 'created_at'];
+
+    protected static $logAttributes = ['name'];
+
+    protected static $ignoreChangedAttributes = ['updated_at'];
+
+    protected static $logOnlyDirty = true;
+
+    protected static $logName = 'permissions';
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "You have {$eventName} permissions";
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,11 +3,25 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Models\Role as OriginalRole;
 
 class Role extends OriginalRole
 {
-    use CrudTrait;
+    use CrudTrait, LogsActivity;
 
     protected $fillable = ['name', 'guard_name', 'updated_at', 'created_at'];
+
+    protected static $logAttributes = ['name'];
+
+    protected static $ignoreChangedAttributes = ['updated_at'];
+
+    protected static $logOnlyDirty = true;
+
+    protected static $logName = 'role';
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "You have {$eventName} role";
+    }
 }
