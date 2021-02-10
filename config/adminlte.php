c036748b52d215b3450663314855684e14b95541
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-10 17:42:08 +0400

    fix home url

--- config/adminlte.php
+++ config/adminlte.php
@@ -187,13 +187,13 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => '/',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
     'password_reset_url' => 'password/reset',
     'password_email_url' => 'password/email',
-    'profile_url' => false,
+    'profile_url' => false
 
     /*
     |--------------------------------------------------------------------------
