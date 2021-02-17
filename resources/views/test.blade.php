0a48214d4b70d80a45c41ee76f15f278d4a2f588
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-17 18:43:13 +0400

    remove unused views

--- resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.dashboard')
-
-@section('inside')
-    <div style='margin-left: auto; margin-right: auto'>
-        <iframe src="{{$url}}" style='width: 85% ; height: 90vh;border: none'/>
-    </div>
-@endsection
