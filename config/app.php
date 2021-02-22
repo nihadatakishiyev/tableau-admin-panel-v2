1b5cfc4ac46bd0eb142ab504d92e259b44844071
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-02-22 17:34:44 +0400

    remove debugbar from app.php

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
