43299b0f3d5cf03916d2e58b047d45907fe33066
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-03-17 12:01:34 +0400

    make embed object height responsive

--- resources/views/renderView.blade.php
+++ resources/views/renderView.blade.php
@@ -6,18 +6,14 @@
             padding: 0;
         }
     </style>
-    <script type="text/javascript" src="http://192.168.20.213/javascripts/api/viz_v1.js"></script>
-{{--    <div class='tableauPlaceholder' style='width: 100px; height: 950px;margin-left: auto; margin-right: auto'>--}}
-        <object class="tableauViz" width="100%" height="872" style="border:none; padding-top: 0">
+        <script type="text/javascript" src="http://192.168.20.213/javascripts/api/viz_v1.js"></script>
+        {{--    <div class='tableauPlaceholder' style='width: 100px; height: 950px;margin-left: auto; margin-right: auto'>--}}
+        <object class="tableauViz " width="100%" height="" style="border:none; padding-top: 0; min-height: 90vh">
+{{--            <object class="tableauViz " width="100%" height="1000px" style="border:none; padding-top: 0;">--}}
             <param name="path" value={{$url}} />
             <param name="tabs" value="no"/>
             <param name='toolbar' value='top'/>
             <param name='showShareOptions' value='false' />
             <param name='tooltip' value='no'/>
         </object>
-{{--    </div>--}}
-    {{--    <div class="tableauPlaceholder" style='margin-left: auto; margin-right: auto'>--}}
-    {{--        <iframe src="{{$url}}" style='width: 100% ; height: 100vh;border: none'/>--}}
-    {{--    </div>--}}
-{{--        {{$url}}--}}
 @stop
