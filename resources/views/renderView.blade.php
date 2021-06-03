ce29b16b463dbcae42823f0ec48158a613b282a7
nihadatakishiyev <niko.atakishiyev@gmail.com>
2021-06-03 10:50:36 +0400

    increase min vh for view:

--- resources/views/renderView.blade.php
+++ resources/views/renderView.blade.php
@@ -8,7 +8,7 @@
     </style>
         <script type="text/javascript" src="http://192.168.20.213/javascripts/api/viz_v1.js"></script>
         {{--    <div class='tableauPlaceholder' style='width: 100px; height: 950px;margin-left: auto; margin-right: auto'>--}}
-        <object class="tableauViz " width="100%" height="" style="border:none; min-height: 93vh">
+        <object class="tableauViz " width="100%" height="" style="border:none; min-height: 92vh">
 {{--            <object class="tableauViz " width="100%" height="1000px" style="border:none; padding-top: 0;">--}}
             <param name="path" value={{$url}} />
             <param name="tabs" value="no"/>
